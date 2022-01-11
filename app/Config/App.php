e96e341584d2fa8555f856e6a70abbf64e5b7c3b
Andrei Pivoda <andrei.pivoda@gmail.com>
2022-01-11 20:42:30 +0200

    change location

--- app/Config/App.php
+++ app/Config/App.php
@@ -23,7 +23,7 @@ class App extends BaseConfig
      *
      * @var string
      */
-    public $baseURL = 'https://web-helper.andreipivoda.ro/';
+    public $baseURL = 'https://andreipivoda.ro/backend/';
 
     /**
      * --------------------------------------------------------------------------
--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -19,9 +19,7 @@ class MessageController extends BaseController
     public function index($arg)
     {
  
-        header('Access-Control-Allow-Origin: *');
-        header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-        header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+     
         $obj = (object) null;
         $obj->function = $arg;
         $obj->message = file_get_contents("php://input");
