4f078294c8fdb3754c8d37133305a20f50a83ac2
Andrei Pivoda <andrei.pivoda@gmail.com>
2022-01-11 23:15:02 +0200

    test email

--- app/Controllers/MessageController.php
+++ app/Controllers/MessageController.php
@@ -12,18 +12,28 @@ class MessageController extends BaseController
 {
     use ResponseTrait;
 
-    
+
 
 
 
     public function index($arg)
     {
- 
-     
+
+
         $obj = (object) null;
         $obj->function = $arg;
         $obj->message = file_get_contents("php://input");
 
+
+        $email = \Config\Services::email();
+
+        $email->setFrom('contact@andreipivoda.ro', 'Web Message');
+        $email->setTo('contact@andreipivoda.ro');
+
+        $email->setSubject('Web Message');
+        $email->setMessage(json_encode($obj->message));
+
+        $email->send();
 
 
         log_message('info', json_encode($obj));
